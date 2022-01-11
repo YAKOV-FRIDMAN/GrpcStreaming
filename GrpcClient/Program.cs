8d00078f68fc1fa238df2b042d8408a39abaf280
yakov fridman <kobi2960@gmail.com>
2022-01-11 23:37:18 +0200

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcStreaming;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            using var chanel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new StreamService.StreamServiceClient(chanel);
+            var userName = "kobi";
+            using var  stream  = client.StartStreaming();
+            var respone = Task.Run(async () => {
+                await foreach (var item in stream.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(item.Message);
+                }
+            });
+            Console.WriteLine("enteer message to stream to server");
+            while (true)
+            {
+                var text = Console.ReadLine();
+                if (string.IsNullOrEmpty(text))
+                    break;
+                await stream.RequestStream.WriteAsync(new StrameMessage()
+                {
+                    Message = text,
+                    Username = userName,
+                });
+            }
+            Console.WriteLine("disconect...");
+            await stream.RequestStream.CompleteAsync();
+            await respone;
+        }
+    }
+}
---
+++ GrpcStreaming/Services/MessageStreamService.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcStreaming.Services
+{
+    public class MessageStreamService : StreamService.StreamServiceBase
+    {
+        public ILogger<MessageStreamService> Logger;
+        public MessageStreamService(ILogger<MessageStreamService> logger)
+        {
+            Logger = logger;
+        }
+        public override async Task StartStreaming(IAsyncStreamReader<StrameMessage> requestStream, IServerStreamWriter<StrameMessage> responseStream, ServerCallContext context)
+        {
+            if (requestStream != null)
+            {
+                if (!await requestStream.MoveNext())
+                    return;
+            }
+            try
+            {
+                if (!string.IsNullOrEmpty(requestStream.Current.Message))
+                {
+                    if (string.Equals(requestStream.Current.Message, "qw!", StringComparison.OrdinalIgnoreCase))
+                        return;
+
+                }
+                Console.WriteLine($"message from cleint: {requestStream.Current.Username}, message: {requestStream.Current.Message}");
+                await responseStream.WriteAsync(new StrameMessage()
+                {
+                    Username = requestStream.Current.Username,
+                    Message = $"Replay strem from server @: {DateTime.UtcNow:f} to {requestStream.Current.Username}"
+                });
+            }
+            catch (Exception e)
+            {
+                Logger.LogInformation(e.Message);
+            }
+        }
+    }
+}
